e0f963875b9b02406b17ff3bdb5dcdeaba8837fa
fanXvirat <142383812+fanXvirat@users.noreply.github.com>
2026-06-07 22:00:47 +0530

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,6 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://jeeforces.vercel.app/sitemap.xml',
+    sitemap: 'https://jeeforces.tech/sitemap.xml',
   };
 }
